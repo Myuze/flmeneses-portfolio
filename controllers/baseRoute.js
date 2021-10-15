54179a8c25d4d06bd080045c5d4855362ec56c76
Myuze <Myuze@users.noreply.github.com>
2021-10-15 00:59:51 -0700

    Added Techstack handlebars column

--- controllers/baseRoute.js
+++ controllers/baseRoute.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 
 router.get('/', async (req, res) => {
   try {
-    res.status(200).render('main');
+    res.status(200).render('home');
   } catch (err) {
     console.log(err);
     res.status(500).render('404');
