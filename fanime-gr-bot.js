d5b437a7ccef1e44541ba10760a09afdcc22156d
Florian Meneses <flmeneses.dev@gmail.com>
2022-04-29 01:55:11 -0700

    Add gr-bot

---
+++ fanime-gr-bot.js
@@ -0,0 +1,29 @@
+const { App } = require('@slack/bolt');
+
+// Initializes your app with your bot token and signing secret
+const app = new App({
+  token: process.env.SLACK_BOT_TOKEN,
+  signingSecret: process.env.SLACK_SIGNING_SECRET,
+  socketMode: true,
+  appToken: process.env.SLACK_APP_TOKEN,
+});
+
+// Listens to incoming messages that contain "hello"
+app.message('hello', async ({ message, say }) => {
+  // say() sends a message to the channel where the event was triggered
+  await say(`Hey there <@${message.user}>!`);
+});
+
+function postKerfuffle(user) {}
+
+app.message('fanime', async ({ message, say }) => {
+  await say('Fanime is pretty awesome!');
+  postKerfuffle();
+});
+
+(async () => {
+  // Start your app
+  await app.start(process.env.PORT || 3000);
+
+  console.log('⚡️ Bolt app is running!');
+})();
